3d08b7d7182feaf10854194b2de07a1b2d634b5a
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2024-04-23 23:32:55 +0300

    Energy vs Power

---
+++ Energy vs Power.jl
@@ -0,0 +1,41 @@
+using Plots
+using CSV
+using DataFrames
+using NumericalIntegration
+using NativeFileDialog
+plotlyjs()
+
+function PowerEnergy_CorD(df,i,p)
+    x=df."Corrected time (s)"
+    y=df."WE(1).Potential (V)"
+
+    E=[0.]
+    P=[0.]
+    Integral=integrate(x,y)
+    V=maximum(y)
+    t=maximum(x)
+    C=(2*i*Integral)/((V^2)*p)
+    E[1]=(C*V^2)/2
+    P.=E./t
+    PE_plot=scatter!(P,E,dpi=360,
+    xlabel="Energy (W*s/g)",ylabel="Power (W)",
+    framestyle=:box,linewidth=2,
+    right_margin=7*Plots.mm,top_margin=5*Plots.mm,
+    legend=false)
+
+end
+
+function pick_PE(i,name,p)
+    PE_overlay=pick_file()
+    df=CSV.read(PE_overlay, DataFrame)
+    Plot=PowerEnergy_CorD(df,i,p)
+    display(Plot)
+    savefig(Plot,name*"_PE.html")
+end
+
+nb=7
+plot()
+
+for i in 1:nb
+    pick_PE(0.0005,"test_PE",33.525e-6)
+end
