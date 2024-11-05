3db984cefee0507024bfaf2f4fda218c3912d45e
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2024-11-05 23:34:18 +0200

    attempt successful

--- Derivative attempt.jl
+++ Derivative attempt.jl
@@ -13,13 +13,22 @@ plotly()
 f=pick_file()
 df=CSV.read(f,DataFrame)
 x=df."Frequency (Hz)"
+x1=sortperm(x)
 y=df."-Phase (°)"
-A=AkimaInterpolation(y,x)
+y1=sortperm(y)
+A=CubicSpline(y[x1],x[x1])
+println(x[x1])
+print(y[x1])
 u=[5, 10, 100, 600, 4]
 t=[20, 500, 760, 54, 422]
 plot(t,u)
 B=LinearInterpolation(u,t)
 plot(x,y,xscale=:log10)
 plot(B)
-plot(A)
+plot(10 .^range(-2, 5, length=10000), x->A(x),xscale=:log10,legend=false)
+plot(10 .^range(-2, 5, length=10000), x->DataInterpolations.derivative(A,x,1),xscale=:log10,
+legend=false)
+
+scatter(x[x1],y[x1],xscale=:log10)
+
 
