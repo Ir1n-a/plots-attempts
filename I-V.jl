cb41d1cfcb440b0d0d39f2383f9a35f95f437cba
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-04-24 01:36:23 +0300

    I-V plots

--- I-V.jl
+++ I-V.jl
@@ -4,6 +4,14 @@ using DataFrames
 using NativeFileDialog
 using DelimitedFiles
 using DataInterpolations
+plotly()
+
+function plot_IV_format()
+    plot(dpi=360,framestyle=:box,
+    right_margin=7*Plots.mm,linewidth=4,
+    formatter=:plain,top_margin=5*Plots.mm,
+    legend=false)
+end
 
 function sort_files()
     fl=pick_folder()
@@ -38,12 +46,34 @@ function sort_files_for_Real()
         push!(F,df)
         push!(files_labels,basename(file))
     end
+
     @show files_vector
     @show files_labels
     @show permutedims(final_files)
-    #return F
-    
+    return F,files_labels
 end
+
+function I_V()
+    File_df,files_labels=sort_files_for_Real()
+    plot_IV=plot_IV_format()
+
+    for (i,df) in enumerate(File_df)
+        Potential=df."Potential applied (V)"
+        Current=df."WE(1).Current (A)"
+
+        plot_IV=plot!(plot_IV,Potential,Current,xlabel="Potential (V)",
+        ylabel="Current (A)",lw=3,hover=files_labels[i])
+    end
+
+    save_folder=pick_folder()
+    savefig(plot_IV,joinpath(save_folder,basename(save_folder)*"_I_V.html"))
+end
+
+I_V()
+
+
+
+
 
 sort_files()
 sort_files_for_Real()
