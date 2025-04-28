99f62155b6a5e4497ba3443b1616244f0b2213bb
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-04-28 22:55:24 +0300

    moreeee

--- I-V 4 normies.jl
+++ I-V 4 normies.jl
@@ -17,6 +17,7 @@ function I_V_normies(n)
     files_vector=[]
     df_vector=[]
     plot_IV=plot_default()
+    p_intp=plot_default()
 
     for i in 1:n
         f=pick_file()
@@ -25,12 +26,19 @@ function I_V_normies(n)
         Potential=df."Potential applied (V)"
         Current=df."WE(1).Current (A)"
 
-        plot_IV=plot!(plot_IV,Potential,Current,xlabel="Potential (V)",
-        ylabel="Current (A)",lw=3,hover=i)
+        Intp_IV=CubicSpline(Current,Potential)
+        
+        p_intp=plot!(range(first(Potential),last(Potential),length=5000),x->Intp_IV(x) ,legend=false,aspect_ratio=1,
+        hover=i)
+
+        #plot_IV=plot!(plot_IV,Potential,Current,xlabel="Potential (V)",
+       # ylabel="Current (A)",lw=3,hover=i)
     end
 
     save_folder=pick_folder()
-    savefig(plot_IV,joinpath(save_folder,basename(save_folder)*"_I_V_normie.html"))
+    #savefig(plot_IV,joinpath(save_folder,basename(save_folder)*"_I_V_normie.html"))
+    savefig(p_intp,joinpath(save_folder,basename(save_folder)*"_I_V_intp.html"))
+
 
     @show files_vector
     #@show df_vector
