f1bd944b602add59be8b1f993de312d59d1e2b13
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-05-14 03:23:42 +0300

    more plots, legend doesn't work on plotly 😭

--- I-V 4 normies.jl
+++ I-V 4 normies.jl
@@ -5,19 +5,20 @@ using CSV
 using DelimitedFiles
 using DataInterpolations
 plotly()
+#gr()
 
-function plot_default()
+function plot_default(p)
     plot(dpi=360,framestyle=:box,
     right_margin=7*Plots.mm,linewidth=4,
     formatter=:plain,top_margin=5*Plots.mm,
-    legend=false)
+    legend=false,legendcolumns=p)
 end
 
 function I_V_normies(n,mode)
     files_vector=[]
     df_vector=[]
-    plot_IV=plot_default()
-    p_intp=plot_default()
+    plot_IV=plot_default(1)
+    p_intp=plot_default(1)
 
     for i in 1:n
         f=pick_file()
@@ -25,33 +26,36 @@ function I_V_normies(n,mode)
         df=CSV.read(f,DataFrame)
         Potential=df."Potential applied (V)"
         Current=df."WE(1).Current (A)"
+        Time=df."Time (s)"
 
         Intp_IV=CubicSpline(Current,Potential)
 
-        if mode== "basic"
+        if mode == "basic"
             plot_IV=plot(plot_IV,Potential,Current,xlabel="Potential (V)",
-            ylabel="Current (A)",lw=3,hover=i)
+            ylabel="Current (A)",lw=3,hover=basename(files_vector[i]),
+            xticks=[0,round(0.25*last(Potential);digits=1),round(0.5*last(Potential);digits=1),
+            round(0.75*last(Potential),digits=1),round(last(Potential),digits=1)])
+        
         elseif mode == "log"
             plot_IV=plot(plot_IV,Potential,Current,xlabel="Potential (V)",
-            ylabel="Current (A)",lw=3,hover=i,yscale=:log10)
+            ylabel="Current (A)",lw=3,hover=basename(files_vector[i]),yscale=:log10,
+            xticks=[0,round(0.25*last(Potential);digits=1),round(0.5*last(Potential);digits=1),
+            round(0.75*last(Potential),digits=1),round(last(Potential),digits=1)])
+        
         else plot_IV=plot!(range(first(Potential),last(Potential),length=5000),
-            x->Intp_IV(x) ,legend=false,aspect_ratio=1,lw=3, hover=i)
+            x->Intp_IV(x) ,legend=false,aspect_ratio=1,lw=3, hover=basename(files_vector[i]),
+            xticks=[0,round(0.25*last(Potential);digits=1),round(0.5*last(Potential);digits=1),
+            round(0.75*last(Potential),digits=1),round(last(Potential),digits=1)],
+            xlabel="Potential (V)",ylabel="Current (A)")
         end
-
-
-        #p_intp=plot!(range(first(Potential),last(Potential),length=5000),x->Intp_IV(x) ,legend=false,aspect_ratio=1,
-        #hover=i)
-
         
     end
 
     save_folder=pick_folder()
     savefig(plot_IV,joinpath(save_folder,basename(save_folder)*"_I_V_$mode.html"))
-    #savefig(p_intp,joinpath(save_folder,basename(save_folder)*"_I_V_intp.html"))
-
-
+    
     @show files_vector
     #@show df_vector
 end
 
-I_V_normies(4,"intp")
+I_V_normies(10,"log")
