a85eee361ba076a3edc03d74261e95ee7a1e7e63
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-06-03 04:13:40 +0300

    models models models 🤪

--- Parameter Try-outs qm.jl
+++ Parameter Try-outs qm.jl
@@ -1,28 +1,43 @@
 using Plots
+using DataFrames
+using CSV
+using NativeFileDialog
 plotly()
 
 function formula_parallel(R1,C1,R2,C2)
     teta1=R1*C1 
     teta2=R2*C2 
-    f=[0.01:0.01:10;11:2:2000;2001:50:100001]
-    f_fs=reverse(f)
+    #f=[0.01:0.01:20;21:2:2000;2001:50:100001]
+    #f_fs=reverse(f)
+
+    F=pick_file()
+    df=CSV.read(F,DataFrame)
+    f=df."Frequency (Hz)"
 
     ratio = teta2/teta1
     @show ratio
 
-    Zre= (R1 ./(1 .+ (2*π*f_fs*R1*C1).^2))  .+ R2 ./(1 .+ (2*π*f_fs*R2*C2).^2)
+    Zre= 2.74 .+ (R1 ./(1 .+ (2*π .*f .* R1*C1).^2)) # .+ R2 ./(1 .+ (2*π*f_fs*R2*C2).^2)
 
-    Zimg= (2*π*f_fs.*(R1 .^2) * C1) ./ (1 .+ (2*π*f_fs*R1 * C1).^2)  .+ 
-    (2*π*f_fs.*(R2.^2) * C2) ./ (1 .+ (2*π*f_fs*R2 * C2).^2)
+    Zimg= (2*π .*f .* (R1 .^2) * C1) ./ (1 .+ (2*π.*f .*R1 * C1).^2)  #.+ 
+    #(2*π*f_fs.*(R2.^2) * C2) ./ (1 .+ (2*π*f_fs*R2 * C2).^2)
+
+    phase=rad2deg.(atan.(Zimg,Zre))
 
     #@show f_fs
     @show first(Zimg),last(Zimg)
     @show first(Zre),last(Zre)
-    scatter(Zre,Zimg,hover=f_fs)
+    scatter(Zre,Zimg)
+    
+    #scatter(f_fs,phase,xscale=:log10,legend=false)
+    @show rad2deg(atan(last(Zimg)/last(Zre)))
+    @show rad2deg(atan(first(Zimg)/first(Zre)))
+    plot(Zre,Zimg)
+    plot(f,phase,xscale=:log10,legend=false)
+    
 end
 
     
-formula_parallel(1000,0.00001,5000,0.0001)
+formula_parallel(10,0.000001,1000,0.00001)
     
-
 
