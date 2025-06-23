fe4ec559ca9f8e92a08b102b9ce6cc152cc51506
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-06-23 23:17:54 +0300

    getting started

--- Makie_attempts.jl
+++ Makie_attempts.jl
@@ -4,6 +4,11 @@ using CairoMakie
 seconds=1:1:10
 Physical_unit=[1090.5,2990,400.6,7,9009, 10000,290076,40005,30490,432532]
 
-scatter(seconds,Physical_unit,rasterize=20,markersize=10)
-lines!(seconds,exp.(seconds))
-current_figure()
+f=Figure()
+acxe=Axis(f[7,4],xlabel="x",ylabel="y",title="yuh")
+scatter!(seconds,Physical_unit,rasterize=50,markersize=30)
+lines!(seconds,exp.(seconds),rasterize=50)
+lines!(1:0.5:10,sin)
+fs=current_figure()
+current_axis()
+save("pffh.png",fs)
