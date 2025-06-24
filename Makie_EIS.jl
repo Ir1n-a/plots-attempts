f1763144212998570dd2f571c7c425776997386c
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-06-24 22:03:19 +0300

    can one even measure an ideal R || C circuit ?

--- Makie_EIS.jl
+++ Makie_EIS.jl
@@ -1,10 +1,10 @@
-using CairoMakie
+using WGLMakie
 using DataFrames
 using CSV
 using NativeFileDialog
 using DataInterpolations
 
-function EIS()
+function EIS(R,C,Ro)
     fls=[]
     fl=pick_file()
     push!(fls,fl)
@@ -18,13 +18,22 @@ function EIS()
     Phase=df."-Phase (°)"[idx]
 
     fig=Figure()
-    acs1=Axis(fig[1,1], aspect = AxisAspect(1))
-    acs2=Axis(fig[1,2],xscale=log10, aspect = AxisAspect(1))
+    #acs1=Axis(fig[1,1], aspect = AxisAspect(1))
+    acs2=Axis(fig[1,1],xscale=log10, aspect = AxisAspect(1))
 
-    Nyquist=lines!(acs1,Zre,Zimg)
-    Bode_Phase=lines!(acs2,Frequency,Phase)
+    #Nyquist=lines!(acs1,Zre,Zimg)
+    #Bode_Phase=lines!(acs2,Frequency,Phase)
+
+    
+    Zre_t= Ro .+ R ./ (1 .+ (2 .* π .* Frequency .* R .* C) .^2)
+    Zimg_t=(2 .* π .* Frequency .* (R .^2) .*C) ./ (1 .+ (2 .* π .* Frequency .* R .* C) .^2)
+    tg= Zimg_t ./ Zre_t
+    angle=rad2deg.(atan.(tg))
+
+    lines!(acs2,Frequency,angle)
+
 
     current_figure()
 end
 
-EIS()
+EIS(10,0.000001,2)
