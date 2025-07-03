dc85dff7195a02df3fae968704e411f0f25295d2
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-07-03 23:40:05 +0300

    uuu, time domain

--- One R series One C.jl
+++ One R series One C.jl
@@ -8,8 +8,8 @@ fl=pick_file()
 df_f=CSV.read(fl,DataFrame, delim=";")
 fs=pick_file()
 df_EIS=CSV.read(fs,DataFrame)
-names(df_EIS)
-df[!, ["Potential (AC) (V)","Current (AC) (A)"]]
+names(df_f)
+df_f[!, ["Potential (AC) (V)","Current (AC) (A)","Time domain (s)","Frequency (Hz)"]]
 df_EIS
 names(df_EIS)
 
@@ -18,6 +18,17 @@ f=df_EIS."Frequency (Hz)"
 ReZ=df_EIS."Z' (Ω)"
 C= 1 ./ (2 * π .*f .* Xc)
 
+Potential=df_f."Potential (AC) (V)"
+Current=df_f."Current (AC) (A)"
+Time=df_f."Time domain (s)"
+Particular_Frequency=df_f."Frequency (Hz)"
+
+lines(Time,Potential)
+lines(Time,Current)
+DataInspector(current_figure())
+
+
 lines(f,C,axis=(xscale=log10,))
 lines(ReZ,Xc,axis= (aspect = AxisAspect(1),))
-DataInspector(current_figure())
+
+lines(f,ReZ,axis=(xscale=log10,))
