e8122974397ef4a061851ec495d5151ff5b36abf
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-07-26 22:40:31 +0300

    plots program for single plots ready to mingle

--- Plots_program.jl
+++ Plots_program.jl
@@ -5,24 +5,8 @@ using GLMakie
 using NumericalIntegration
 using DataInterpolations
 
-function plots_format()
-    Axis_Nyquist=Axis(xlabel="Zre (Ω)",ylabel="Zimg (Ω)",
-    title = "Nyquist")
-    Axis_Bode_Phase=Axis(xlabel="Frequency (Hz)",ylabel="Phase Difference (deg)",
-    title = "Bode Phase Difference",xscale=:log10)
-    Axis_Bode_Module=Axis(xlabel="Frequency (Hz)",ylabel="Z (Ω)",
-    title = "Bode Module",xscale=:log10)
-
-    Axis_CV=Axis(xlabel="Potential (V)",ylabel="Current (A)",
-    title ="Cyclic Voltammetry")
-
-    Axis_CD=Axis(xlabel="Time (s)",ylabel ="Potential (V)",
-    title ="Charge/Discharge")
-
-    return Axis_Nyquist,Axis_Bode_Phase,Axis_Bode_Module,Axis_CV,Axis_CD
-end
-
-function single_plot()
+function single_plot_mode_selection()
+    println("pick data file")
     single_file=pick_file()
     df=CSV.read(single_file,DataFrame)
 
@@ -40,10 +24,82 @@ function single_plot()
         mode = "D"
     else println("this type of data is not supported by this program *shrug emoji*")
     end
-
-    println(df[!,end][1])
-    println(df[!,5][1])
-    return mode
+    return mode,single_file,df
 end
 
-single_plot()
+single_plot_mode_selection()
+
+function single_plot(clr)
+    mode,single_file,df=single_plot_mode_selection()
+    println("pick save folder")
+    save_folder=pick_folder()
+
+    if mode == "EIS"
+
+        idx_EIS=df."-Z'' (Ω)" .>0
+        Zre=df."Z' (Ω)"[idx_EIS]
+        Zimg=df."-Z'' (Ω)"[idx_EIS]
+        Frequency=df."Frequency (Hz)"[idx_EIS]
+        Z=df."Z (Ω)"[idx_EIS]
+        Phase=df."-Phase (°)"[idx_EIS]
+
+        plot_Nyquist=lines(Zre,Zimg,axis=(title="Nyquist",xlabel="Zre (Ω)",
+        ylabel="Zimg (Ω)"),color=clr)
+        DataInspector(plot_Nyquist)
+        display(GLMakie.Screen(),plot_Nyquist)
+
+        save(joinpath(save_folder,basename(single_file)*"_Nyquist.png"),plot_Nyquist)
+
+        plot_Bode_Phase=lines(Frequency,Phase,axis=(title="Bode Phase Difference",
+        xlabel="Frequency (Hz)",ylabel="Phase (deg)",
+        xscale=log10),color=clr)
+        DataInspector(plot_Bode_Phase)
+        display(GLMakie.Screen(),plot_Bode_Phase)
+
+        save(joinpath(save_folder,basename(single_file)*"_Bode Phase.png"),plot_Bode_Phase)
+
+        plot_Bode_Module=lines(Frequency,Z,axis=(title="Bode Module",
+        xlabel="Frequency (Hz)",ylabel="Z (Ω)",xscale=log10),color=clr)
+        DataInspector(plot_Bode_Module)
+        display(GLMakie.Screen(),plot_Bode_Module)
+
+        save(joinpath(save_folder,basename(single_file)*"_Bode Module.png"),plot_Bode_Module)
+        
+        #println(basename(single_file))
+    elseif mode == "CV"
+
+        idx_CV= df[!, :Scan] .==2
+        Potential=df."WE(1).Potential (V)"[idx_CV]
+        Current=df."WE(1).Current (A)"[idx_CV]
+
+        plot_CV=lines(Potential,Current,axis=(title="Cyclic Voltammetry",
+        xlabel="Potential (V)",ylabel="Current (A)"),color=clr)
+        DataInspector(plot_CV)
+        display(GLMakie.Screen(),plot_CV)
+
+        save(joinpath(save_folder,basename(single_file)*"_CV.png"),plot_CV)
+
+    elseif mode =="C" || mode =="D"
+        Time=df."Corrected time (s)"
+        Potential=df."WE(1).Potential (V)"
+
+        if mode == "C"
+            plot_C=lines(Time,Potential,axis=(title="Charge",
+            xlabel="Time (s)",ylabel="Potential (V)"),color=clr)
+            DataInspector(plot_C)
+            display(GLMakie.Screen(),plot_C)
+
+        save(joinpath(save_folder,basename(single_file)*"_C.png"),plot_C)
+        
+        elseif mode == "D"
+            plot_D=lines(Time,Potential,axis=(title="Discharge",
+            xlabel="Time (s)",ylabel="Potential (V)"),color=clr)
+            DataInspector(plot_D)
+            display(GLMakie.Screen(),plot_D)
+
+        save(joinpath(save_folder,basename(single_file)*"_D.png"),plot_D)
+        end
+    end
+end
+
+single_plot(:mediumorchid4)
