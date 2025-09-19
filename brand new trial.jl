1ad20e53f2db3a9a54dc04bf4bc89d857f4951f3
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-09-20 02:35:27 +0300

    bug in the software 😭

--- brand new trial.jl
+++ brand new trial.jl
@@ -4,11 +4,23 @@ using NativeFileDialog
 using Plots
 
 function import_files()
+    println("pick folder of frequency data")
     fldr=pick_folder()
     freq_files=[]
 
+
+    println("pick file of EIS data")
+    file_EIS=pick_file()
+
     max_current=[]
     specific_freq=[]
+
+    vector_sorted_freq=[]
+
+    df_EIS=CSV.read(file_EIS,DataFrame)
+    idx=df_EIS."-Z'' (Ω)" .<0
+    Frequencies=df_EIS."Frequency (Hz)"
+    removed_frequencies=Frequencies[idx]
 
     for file in readdir(fldr,join=true)
 
@@ -16,7 +28,9 @@ function import_files()
         
         if file_ext[end] =="txt"
             df=CSV.read(file,DataFrame)
-            push!(freq_files,df)
+            if df."Frequency (Hz)"[1] ∉ removed_frequencies
+                push!(freq_files,df)
+            end
         end
     end
 
@@ -30,8 +44,27 @@ function import_files()
         push!(specific_freq,freq)
     end
 
+    sorted_freq=sortperm(specific_freq)
+
     @show max_current
     @show specific_freq
+    @show sorted_freq
+
+    @show specific_freq[sorted_freq]
+
+    issorted(specific_freq[sorted_freq])
+
+    final_freq=specific_freq[sorted_freq]
+    final_current=max_current[sorted_freq]
+
+    @show final_freq
+    @show final_current
+    @show removed_frequencies
+
+    #issorted(final_current)
+
+
+    scatter(final_freq,final_current)
 
     #@show freq_files
 end
