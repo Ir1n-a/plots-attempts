285865dfb7af55372e016ea763eca513a42a4d8e
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-09-24 04:32:12 +0300

    nuh uh

--- brand new trial.jl
+++ brand new trial.jl
@@ -2,20 +2,21 @@ using DataFrames
 using CSV
 using NativeFileDialog
 using Plots
+using SymbolicRegression
+using MLJ
 
 function import_files()
     println("pick folder of frequency data")
     fldr=pick_folder()
     freq_files=[]
 
-
     println("pick file of EIS data")
     file_EIS=pick_file()
 
-    max_current=[]
+    Current=[]
+    Potential=[]
+    Frequency=[]
     specific_freq=[]
-
-    vector_sorted_freq=[]
 
     df_EIS=CSV.read(file_EIS,DataFrame)
     idx=df_EIS."-Z'' (Ω)" .<0
@@ -23,50 +24,69 @@ function import_files()
     removed_frequencies=Frequencies[idx]
 
     for file in readdir(fldr,join=true)
-
-        file_ext=split(basename(file),".")
         
-        if file_ext[end] =="txt"
-            df=CSV.read(file,DataFrame)
+        df=CSV.read(file,DataFrame)
             if df."Frequency (Hz)"[1] ∉ removed_frequencies
                 push!(freq_files,df)
             end
-        end
-    end
-
-    for i in eachindex(freq_files)
-        df=freq_files[i]
-
-        current=maximum(df."Current (AC) (A)")
-        freq=first(df."Frequency (Hz)")
-
-        push!(max_current,current)
-        push!(specific_freq,freq)
+        push!(Current,df."Current (AC) (A)")
+        push!(Potential,df."Potential (AC) (V)")
+        push!(Frequency,df."Frequency (Hz)"[1])
     end
 
     sorted_freq=sortperm(specific_freq)
 
-    @show max_current
-    @show specific_freq
-    @show sorted_freq
+    #@show freq_files
+    #@show removed_frequencies
 
-    @show specific_freq[sorted_freq]
-
-    issorted(specific_freq[sorted_freq])
-
-    final_freq=specific_freq[sorted_freq]
-    final_current=max_current[sorted_freq]
-
-    @show final_freq
-    @show final_current
-    @show removed_frequencies
-
-    #issorted(final_current)
-
-
-    scatter(final_freq,final_current)
+    return Frequency,Potential,Current
 
     #@show freq_files
 end
 
 import_files()
+
+function interdimensionalpolation()
+    Frequency,Potential,Current=import_files()
+
+    possible_equations=[]
+
+    model=SRRegressor()
+
+    for i in eachindex(Frequency)
+
+    mach=machine(model,reshape(Potential[i], (length(Potential[i]),1)),Current[i])
+    fit!(mach)
+
+    r=report(mach)
+    result=r.equations[r.best_idx]
+    push!(possible_equations,result)
+    end
+
+    return possible_equations
+end
+
+
+interdimensionalpolation()
+
+
+    import_files()
+
+using SymbolicRegression
+using MLJ
+
+x = #input (n,1) matrix
+y = # output
+
+model = SRRegressor(
+    binary_operators=[+, -, *, /],
+    unary_operators=[cos],
+    niterations=30
+)
+mach = machine(model, X, y)
+fit!(mach)
+
+r = report(mach)
+r
+
+r.equations[r.best_idx]
